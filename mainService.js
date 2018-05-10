d06d3ea3af276c7a8220dc78e4749e33f4b03d22
kamilnaja <kamilnaja@gmail.com>
2018-05-10 23:19:09 +0200

    added fn to load items

--- mainService.js
+++ mainService.js
@@ -1,10 +1,17 @@
-app.service('ServiceWithMethods', function ($resource) {
+// @ts-check
+app.service('contactService', function ($resource) {
     var service = {
+
         get getData() {
             return $resource('/contacts')
-        }
+        },
+
+        delete() {
+            return Contact.remove({ id: 3 });
+
+        },
+
+
     }
     return service
 })
-
-
--- script.js
+++ script.js
@@ -1,19 +1,40 @@
+// @ts-check
 var app = angular.module('app', [
     'ngResource',
     'ngMockE2E'
 ]);
 
 app.component('secondComponent', {
-    controller: function ($scope, ServiceWithMethods) {
-        $scope.contactsData = ServiceWithMethods.getData;
-        $scope.contacts = $scope.contactsData.query();
+    controller: function ($scope, contactService) {
+
+        $scope.deleteItem = function () {
+            $scope.contactsData.delete()
+                .$promise
+                .then(updateContactList)
+        }
+
+        function updateContactList() {
+            $scope.contacts = $scope.getContacts();
+        }
+
+        $scope.loadContactList = function () {
+            $scope.contactsData = contactService.getData;
+            $scope.contacts = $scope.contactsData.query();
+        }
+
     },
     template:
         `
         <h2>Second component</h2>
+        <button ng-click="loadContactList()">Get items </button>
         <div>
             <ul ng-repeat="contact in contacts">
                 <li>{{contact.id}} | {{contact.name}} | {{contact.phone}}</li>
             </ul>
-        </div>`
+        </div>
+        <div>
+            <button ng-click="deleteItem()">Click to delete something</button>
+        </div>
+        
+        `
 })
